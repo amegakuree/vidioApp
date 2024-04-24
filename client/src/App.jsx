af3f4caedbdcd836603c65548f72205c86488535
iqbal1704 <iqbalmipa17@gmail.com>
2024-04-24 22:13:14 +0800

    Updateing Routing app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Register from "./components/register";
-import Login from "./components/login";
+import Register from "./component/Register";
+import Login from "./components/Login";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import { VideoRoom } from "./components/VideoPlayer";
+import Register from './components/Register';
 function App() {
   const router = createBrowserRouter([
     {
@@ -27,6 +29,16 @@ function App() {
         <>
           <Navbar />
           <Home />
+        </>
+      ),
+    },
+    {
+      path: "/room",
+      element: (
+        <>
+          <Navbar />
+          <Home />
+          <VideoRoom />
         </>
       ),
     },
